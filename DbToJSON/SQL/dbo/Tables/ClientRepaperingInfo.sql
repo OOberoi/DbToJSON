5b719c5d19184fca49eca721ecdea1760666e402
Obi Oberoi <obi@eoberoi.com>
2021-09-30 18:49:15 -0400

    added sql table

---
+++ DbToJSON/SQL/dbo/Tables/ClientRepaperingInfo.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[ClientRepaperingInfo] (
+    [ID]                BIGINT         IDENTITY (1, 1) NOT NULL,
+    [PackageID]         NCHAR (50)     NULL,
+    [ClientID]          NCHAR (10)     NULL,
+    [PackageInstanceID] NCHAR (50)     NULL,
+    [Comments]          NVARCHAR (250) NULL,
+    [JSON]              NVARCHAR (MAX) NULL,
+    [DateCreated]       DATETIME       NULL,
+    [DateUpdated]       DATETIME       NULL,
+    CONSTRAINT [PK_ClientRepaperingInfo] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
