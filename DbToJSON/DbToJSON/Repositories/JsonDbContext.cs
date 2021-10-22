92185e15a9967c75e75a887a97c3854cfff174ff
Obi Oberoi <obi@eoberoi.com>
2021-10-21 22:14:40 -0400

    added connstring to dbContext

--- DbToJSON/DbToJSON/Repositories/JsonDbContext.cs
+++ DbToJSON/DbToJSON/Repositories/JsonDbContext.cs
@@ -15,6 +15,12 @@ namespace DbToJSON.Repositories
         {
         }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-9AB4882; Database=DbToJSON; trusted_connection=true;");
+            base.OnConfiguring(optionsBuilder);
+        }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
