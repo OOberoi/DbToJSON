421f2520b71b049e5575dd48663963a72f930edb
Obi Oberoi <obi@eoberoi.com>
2021-11-15 19:48:24 -0500

    added backslashes

--- DbToJSON/UnitTests/UnitTests.cs
+++ DbToJSON/UnitTests/UnitTests.cs
@@ -16,6 +16,7 @@ namespace UnitTests
     public class DbToJSONUnitTests
     {
         private static readonly string hyphen = "_";
+        private static readonly string backSlashes = "\\";
         private static readonly string year = DateTime.Now.Year.ToString();
         private static readonly string month = DateTime.Now.Date.Month.ToString();
         private static readonly string day = DateTime.Now.Date.Day.ToString();
