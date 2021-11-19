3e11bb7ba5a43c286f2f80eb28d134d100766224
Obi Oberoi <obi@eoberoi.com>
2021-11-18 22:36:15 -0500

    added a namespace to the interface class

--- DbToJSON/DbToJSON/Repositories/IRepaperingInfo.cs
+++ DbToJSON/DbToJSON/Repositories/IRepaperingInfo.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using DbToJSON.Shared;
 namespace DbToJSON.Repositories
 {
     public interface IRepaperingInfo
     {
-
+        //IEnumerable<client>
         string GetJSON();
         string GetJSON(string[] instanceArr);
         IList<string> GetJSONList(string[] instanceArr);
