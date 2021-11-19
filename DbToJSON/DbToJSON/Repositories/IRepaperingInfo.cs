6344d23aace9f34fb8a0c45ce8bf1cb2f43a5e07
Obi Oberoi <obi@eoberoi.com>
2021-11-18 22:37:14 -0500

    added a new method signature

--- DbToJSON/DbToJSON/Repositories/IRepaperingInfo.cs
+++ DbToJSON/DbToJSON/Repositories/IRepaperingInfo.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DbToJSON.Shared;
+
 namespace DbToJSON.Repositories
 {
     public interface IRepaperingInfo
     {
-        //IEnumerable<client>
+        IEnumerable<ClientRepaperingInfo> GetAllRepaperingInfo();
         string GetJSON();
         string GetJSON(string[] instanceArr);
         IList<string> GetJSONList(string[] instanceArr);
