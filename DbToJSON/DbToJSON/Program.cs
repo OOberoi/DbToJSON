130fa63e571acf106a218b25136c6c7fb64fe28c
Obi Oberoi <obi@eOberoi.com>
2021-11-23 22:03:56 -0500

    added region

--- DbToJSON/DbToJSON/Program.cs
+++ DbToJSON/DbToJSON/Program.cs
@@ -8,10 +8,12 @@ namespace DbToJSON
         public static void Main()
         {
             RepaperingInfoRepository _repaperingInfo = new();
-            
+
+            #region "GetJSON"
             string json = _repaperingInfo.GetJSON();
             Console.WriteLine(json);
             Console.ReadLine();
+            #endregion
         }
     }
 }
