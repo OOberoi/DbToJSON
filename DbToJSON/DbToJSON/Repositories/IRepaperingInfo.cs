ffa84a3fac506361f93e17f0c9802d0dd13930fe
Obi Oberoi <obi@eoberoi.com>
2021-11-24 19:54:22 -0500

    reorganized the code

--- DbToJSON/DbToJSON/Repositories/IRepaperingInfo.cs
+++ DbToJSON/DbToJSON/Repositories/IRepaperingInfo.cs
@@ -1,9 +1,5 @@
-﻿using System;
+﻿using DbToJSON.Shared;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DbToJSON.Shared;
 
 namespace DbToJSON.Repositories
 {
