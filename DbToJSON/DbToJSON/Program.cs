436be834182f4a29284570ae7d2c211f52de087d
Obi Oberoi <obi@eOberoi.com>
2021-12-19 20:54:21 -0500

    stubbed out a new method

--- DbToJSON/DbToJSON/Program.cs
+++ DbToJSON/DbToJSON/Program.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
+using System.Collections;
 
 namespace DbToJSON
 {
@@ -251,7 +252,10 @@ namespace DbToJSON
         #endregion
 
         #region GetRepaperingInfo
+        public static IList GetRepaperngInfo()
+        { 
 
+        }
         #endregion
 
     }
