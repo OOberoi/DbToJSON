8031b0835764324b2f2b54d5c3da5fb9efbd308a
Obi Oberoi <obi@eOberoi.com>
2021-12-28 20:15:20 -0500

    adding method signatures

--- DbToJSON/DbToJSON/Repositories/IAsyncRepository.cs
+++ DbToJSON/DbToJSON/Repositories/IAsyncRepository.cs
@@ -8,6 +8,9 @@ namespace DbToJSON.Repositories
 {
     public interface IAsyncRepository<T> where T : class
     {
-
+        Task<T> GetAsync(Guid id);
+        Task<IEnumerable<T>> GetAllAsync(); 
+        Task DeleteAsync(Guid id);  
+            
     }
 }
