3e77774740c86a35c158fcd0fa1d292850768408
Obi Oberoi <obi@eOberoi.com>
2021-12-29 19:54:05 -0500

    start adding business rules

--- DbToJSON/DbToJSON/Validator/CreateEventCommandValidator.cs
+++ DbToJSON/DbToJSON/Validator/CreateEventCommandValidator.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DbToJSON.Events.Commands;
+using FluentValidation;
 
 namespace DbToJSON.Validator
 {
-    public class CreateEventCommandValidator
+    public class CreateEventCommandValidator : AbstractValidator<CreateEventCommand>
     {
+        public CreateEventCommandValidator()
+        {
+            
+        }
     }
 }
