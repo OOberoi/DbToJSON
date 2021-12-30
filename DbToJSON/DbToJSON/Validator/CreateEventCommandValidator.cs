fe60330ab1fb7d8a432820b4403e801da20bea8f
Obi Oberoi <obi@eOberoi.com>
2021-12-29 19:59:04 -0500

    added a rule to the Name property

--- DbToJSON/DbToJSON/Validator/CreateEventCommandValidator.cs
+++ DbToJSON/DbToJSON/Validator/CreateEventCommandValidator.cs
@@ -12,7 +12,10 @@ namespace DbToJSON.Validator
     {
         public CreateEventCommandValidator()
         {
-            
+            RuleFor(p => p.Name)
+                .NotEmpty().WithMessage("{ProperName} is required.")
+                .NotNull()
+                .MaximumLength(50).WithMessage("{PropertyName} must not exceed 50 characters.");
         }
     }
 }
