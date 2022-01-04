fc60af2f6d15d3fad467666c4702cadd1a04743e
Obi Oberoi <obi@eoberoi.com>
2022-01-03 21:53:40 -0500

    added appservices

--- DbToJSON/DbToJSON.Infrastructure/InfraServiceRegistration.cs
+++ DbToJSON/DbToJSON.Infrastructure/InfraServiceRegistration.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DbToJSON.Infrastructure
 {
     public static class InfraServiceRegistration
     {
-        
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        { 
+            //services.AddAu
+        }
     }
 }
