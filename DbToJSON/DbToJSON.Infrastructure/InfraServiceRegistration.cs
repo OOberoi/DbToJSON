52c6566e85d36a750838160d496257bb4e2b9a0b
Obi Oberoi <obi@eOberoi.com>
2022-01-03 21:30:20 -0500

    added a static class

--- DbToJSON/DbToJSON.Infrastructure/InfraServiceRegistration.cs
+++ DbToJSON/DbToJSON.Infrastructure/InfraServiceRegistration.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace DbToJSON.Infrastructure
 {
-    public class InfraServiceRegistration
+    public static class InfraServiceRegistration
     {
+        
     }
 }
