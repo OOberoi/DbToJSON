83892cda9818bf610007b6fbc80636e82fc586a4
Obi Oberoi <obi@eoberoi.com>
2022-01-05 22:06:36 -0500

    enforced data validations

--- DbToJSON/DbToJSON.Application/Models/RegistrationRequest.cs
+++ DbToJSON/DbToJSON.Application/Models/RegistrationRequest.cs
@@ -9,10 +9,22 @@ namespace DbToJSON.Application.Models
 {
     public class RegistrationRequest
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string UserName { get; set; }
-        public string Password { get; set; }
+        [Required]
+        public string? FirstName { get; set; }
+
+        [Required]
+        public string? LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        public string? UserName { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        public string? Password { get; set; }
     }
 }
