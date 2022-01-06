3fa584f5f24f19330dd20a59c6c75fc73a6bba6c
Obi Oberoi <obi@eoberoi.com>
2022-01-05 22:01:27 -0500

    added a namespace

--- DbToJSON/DbToJSON.Application/Models/RegistrationRequest.cs
+++ DbToJSON/DbToJSON.Application/Models/RegistrationRequest.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace DbToJSON.Application.Models
 {
     public class RegistrationRequest
     {
+
     }
 }
