b1d7a8a2313a7d5066ce7ab4e01383109be0c65c
Obi Oberoi <obi@eoberoi.com>
2022-01-05 21:52:29 -0500

    added a bunch of nullable props

--- DbToJSON/DbToJSON.Application/Models/AuthenticationResponse.cs
+++ DbToJSON/DbToJSON.Application/Models/AuthenticationResponse.cs
@@ -8,8 +8,9 @@ namespace DbToJSON.Application.Models
 {
     public class AuthenticationResponse
     {
-        public string Id { get; set; }
-        public string UserName { get; set; }    
-
+        public string? Id { get; set; }
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
+        public string? Token { get; set; }
     }
 }
