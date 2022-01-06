b2474710e45d5eb05c5608f9334d1dc62fe3a698
Obi Oberoi <obi@eoberoi.com>
2022-01-05 21:40:13 -0500

    added couple nullable props

--- DbToJSON/DbToJSON.Application/Models/AuthenticationRequest.cs
+++ DbToJSON/DbToJSON.Application/Models/AuthenticationRequest.cs
@@ -8,6 +8,7 @@ namespace DbToJSON.Application.Models
 {
     public class AuthenticationRequest
     {
-        public string Email { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; } = null;
     }
 }
