d6bcb7f5757e99c765fca9cd5bd18235385213f0
Obi Oberoi <obi@eoberoi.com>
2022-01-05 21:53:51 -0500

    imported a namespace

--- DbToJSON/DbToJSON.Application/Contracts/Identity/IAuthenticationService.cs
+++ DbToJSON/DbToJSON.Application/Contracts/Identity/IAuthenticationService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DbToJSON.Application.Models;
 
 namespace DbToJSON.Application.Contracts.Identity
 {
