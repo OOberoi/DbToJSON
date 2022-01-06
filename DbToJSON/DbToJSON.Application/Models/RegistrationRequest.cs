eafb3805d6e2f6eeb5d4d1db09ea2e366fde8e8a
Obi Oberoi <obi@eoberoi.com>
2022-01-05 22:02:45 -0500

    added a bunch of properties

--- DbToJSON/DbToJSON.Application/Models/RegistrationRequest.cs
+++ DbToJSON/DbToJSON.Application/Models/RegistrationRequest.cs
@@ -9,6 +9,10 @@ namespace DbToJSON.Application.Models
 {
     public class RegistrationRequest
     {
-
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
     }
 }
