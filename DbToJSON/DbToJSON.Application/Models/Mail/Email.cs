2b8c930c1c625f26bd7b1eacf9132b9848c3c269
Obi Oberoi <obi@eoberoi.com>
2022-01-06 22:07:57 -0500

    added props

--- DbToJSON/DbToJSON.Application/Models/Mail/Email.cs
+++ DbToJSON/DbToJSON.Application/Models/Mail/Email.cs
@@ -11,6 +11,6 @@ namespace DbToJSON.Application.Models.Mail
         public string? To { get; set; }
         public string? From { get; set; }
         public string? Subject { get; set; }
-        
+        public string? Body { get; set; }
     }
 }
