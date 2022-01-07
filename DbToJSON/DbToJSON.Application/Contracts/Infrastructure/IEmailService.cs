a32cac3d814b3ff1911633096d91e776cd9d23e9
Obi Oberoi <obi@eoberoi.com>
2022-01-06 21:08:48 -0500

    organized the folder structure

--- DbToJSON/DbToJSON.Application/Contracts/Infrastructure/IEmailService.cs
+++ DbToJSON/DbToJSON.Application/Contracts/Infrastructure/IEmailService.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DbToJSON.Application.Models;
 
 namespace DbToJSON.Application.Contracts.Infrastructure
 {
     public interface IEmailService
     {
+        //Task<bool> SendEmailAsync(Email);
     }
 }
