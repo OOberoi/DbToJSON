778278213f83ee9cf6f942e101c31cd5e0448704
Obi Oberoi <obi@eoberoi.com>
2022-01-07 21:14:59 -0500

    added a method signature

--- DbToJSON/DbToJSON.Application/Contracts/Infrastructure/IEmailService.cs
+++ DbToJSON/DbToJSON.Application/Contracts/Infrastructure/IEmailService.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DbToJSON.Application.Models;
+using DbToJSON.Application.Models.Mail;
 
 namespace DbToJSON.Application.Contracts.Infrastructure
 {
     public interface IEmailService
     {
-        //Task<bool> SendEmailAsync(Email);
+        Task<bool> SendEmailAsync(Email email);
     }
 }
