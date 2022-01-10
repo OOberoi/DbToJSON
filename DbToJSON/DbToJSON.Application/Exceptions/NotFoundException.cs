8d924ea3ffc7eabb4350416dfd04e3a93c11b4e1
Obi Oberoi <obi@eoberoi.com>
2022-01-09 20:10:38 -0500

    added a constructor

--- DbToJSON/DbToJSON.Application/Exceptions/NotFoundException.cs
+++ DbToJSON/DbToJSON.Application/Exceptions/NotFoundException.cs
@@ -8,5 +8,9 @@ namespace DbToJSON.Application.Exceptions
 {
     public class NotFoundException : ApplicationException
     {
+        public NotFoundException(string name, object key) : base($"{name} - {key} is not found!")
+        {
+           
+        }
     }
 }
