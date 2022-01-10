0af43e72d0a9af67b87fe8dfce51158639eb371a
Obi Oberoi <obi@eoberoi.com>
2022-01-09 20:33:46 -0500

    cleaned up the usings

--- DbToJSON/DbToJSON.Application/Exceptions/ValidException.cs
+++ DbToJSON/DbToJSON.Application/Exceptions/ValidException.cs
@@ -1,9 +1,6 @@
-﻿using System;
+﻿using FluentValidation.Results;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FluentValidation.Results;
 
 namespace DbToJSON.Application.Exceptions
 {
