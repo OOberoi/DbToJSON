da6b95dbe4efd47ae559966b9e68badedca4a038
Obi Oberoi <obi@eoberoi.com>
2022-01-09 20:32:01 -0500

    built a validationError object

--- DbToJSON/DbToJSON.Application/Exceptions/ValidException.cs
+++ DbToJSON/DbToJSON.Application/Exceptions/ValidException.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentValidation.Results;
 
 namespace DbToJSON.Application.Exceptions
 {
     public class ValidException : ApplicationException
     {
-        public ValidException()
+        public List<string> ValidationErrors { get; set; }
+        public ValidException(ValidationResult validationResult)
         {
-
+            ValidationErrors = new List<string>();
+            foreach (var validationError in validationResult.Errors)
+            {
+                ValidationErrors.Add(validationError.ErrorMessage);
+            }
         }
     }
 }
