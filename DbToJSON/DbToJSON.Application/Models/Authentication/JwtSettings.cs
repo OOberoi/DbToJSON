f7350dd192ab7b600ed825a56983e978b6a33dc2
Obi Oberoi <obi@eoberoi.com>
2022-01-10 21:46:20 -0500

    imported the namespace

--- DbToJSON/DbToJSON.Application/Models/Authentication/JwtSettings.cs
+++ DbToJSON/DbToJSON.Application/Models/Authentication/JwtSettings.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DbToJSON.Application.Models.Authentication;
 
 namespace DbToJSON.Application.Models.Authentication
 {
