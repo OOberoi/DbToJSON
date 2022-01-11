7d0432b6642d240a9b14a2246b54e6759fa4b30f
Obi Oberoi <obi@eoberoi.com>
2022-01-10 22:28:35 -0500

    added a constructor

--- DbToJSON/DbToJSON.Application/Responses/BaseResponse.cs
+++ DbToJSON/DbToJSON.Application/Responses/BaseResponse.cs
@@ -11,9 +11,10 @@ namespace DbToJSON.Application.Responses
         public bool? Success { get; set; }
         public string? Message { get; set; }
         public List<string>? ValidationErrors { get; set; }
+        
         public BaseResponse()
         {
-            
+            Success = true;    
         }
     }
 }
