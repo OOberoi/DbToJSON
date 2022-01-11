0baf529604e0e59794c31d5516317b1807dab0fe
Obi Oberoi <obi@eoberoi.com>
2022-01-10 21:48:54 -0500

    made props nullable

--- DbToJSON/DbToJSON.Application/Models/Authentication/JwtSettings.cs
+++ DbToJSON/DbToJSON.Application/Models/Authentication/JwtSettings.cs
@@ -9,9 +9,9 @@ namespace DbToJSON.Application.Models.Authentication
 {
     public class JwtSettings
     {
-        public string Key { get; set; }
-        public string Issuer { get; set; }
-        public string Audience { get; set; }
-        public double DurationInMinutes { get; set; }
+        public string? Key { get; set; }
+        public string? Issuer { get; set; }
+        public string? Audience { get; set; }
+        public double? DurationInMinutes { get; set; }
     }
 }
