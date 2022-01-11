9ac99f7b6f1395f7e268762a74763e4a80548d8a
Obi Oberoi <obi@eoberoi.com>
2022-01-10 22:27:37 -0500

    added prop to POCO

--- DbToJSON/DbToJSON.Application/Responses/BaseResponse.cs
+++ DbToJSON/DbToJSON.Application/Responses/BaseResponse.cs
@@ -10,7 +10,7 @@ namespace DbToJSON.Application.Responses
     {
         public bool? Success { get; set; }
         public string? Message { get; set; }
-
+        public List<string>? ValidationErrors { get; set; }
         public BaseResponse()
         {
             
