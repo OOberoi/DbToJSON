87268f02a3991404aac9ab981a307a12a324b9ea
Obi Oberoi <obi@eoberoi.com>
2022-01-15 19:09:21 -0500

    organized usings

--- DbToJSON/DbToJSON.Application/Contracts/Identity/IAuthenticationService.cs
+++ DbToJSON/DbToJSON.Application/Contracts/Identity/IAuthenticationService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DbToJSON.Application.Models;
+﻿using DbToJSON.Application.Models;
 
 namespace DbToJSON.Application.Contracts.Identity
 {
