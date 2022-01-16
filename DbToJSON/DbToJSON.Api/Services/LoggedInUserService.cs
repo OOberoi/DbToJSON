97a4c4d384da346c5d0b81f3a5cd7d6717f1cc7c
Obi Oberoi <obi@eoberoi.com>
2022-01-15 19:26:14 -0500

    added constructor to the class

--- DbToJSON/DbToJSON.Api/Services/LoggedInUserService.cs
+++ DbToJSON/DbToJSON.Api/Services/LoggedInUserService.cs
@@ -4,6 +4,10 @@ namespace DbToJSON.Api.Services
 {
     public class LoggedInUserService : ILoggedInUserService
     {
+        public LoggedInUserService()
+        {
+
+        }
         string ILoggedInUserService.UserId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         string ILoggedInUserService.UserName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
     }
