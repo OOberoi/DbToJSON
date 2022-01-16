50a787452cbfd592fd495196a0157ff38afa9863
Obi Oberoi <obi@eoberoi.com>
2022-01-15 19:22:58 -0500

    implementated the ILoggedInService interface

---
+++ DbToJSON/DbToJSON.Api/Services/LoggedInUserService.cs
@@ -0,0 +1,10 @@
+﻿using DbToJSON.Application.Contracts;
+
+namespace DbToJSON.Api.Services
+{
+    public class LoggedInUserService : ILoggedInUserService
+    {
+        string ILoggedInUserService.UserId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        string ILoggedInUserService.UserName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
