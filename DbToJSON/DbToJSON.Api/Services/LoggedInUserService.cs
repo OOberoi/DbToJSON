71b1fc1c09095aacbc9b175d93a969be375f8b74
Obi Oberoi <obi@eoberoi.com>
2022-01-15 19:33:11 -0500

    LoggedInUserService completion

--- DbToJSON/DbToJSON.Api/Services/LoggedInUserService.cs
+++ DbToJSON/DbToJSON.Api/Services/LoggedInUserService.cs
@@ -1,12 +1,15 @@
 ﻿using DbToJSON.Application.Contracts;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
 
 namespace DbToJSON.Api.Services
 {
     public class LoggedInUserService : ILoggedInUserService
     {
-        public LoggedInUserService()
+        public string? UserId { get; set; }
+        public LoggedInUserService(IHttpContextAccessor httpContextAccessor)
         {
-
+            UserId = httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
         }
         string ILoggedInUserService.UserId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         string ILoggedInUserService.UserName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
