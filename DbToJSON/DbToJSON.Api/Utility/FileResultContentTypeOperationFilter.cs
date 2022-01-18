d418873995418d142daa4bf45f82362493cf40c2
Obi Oberoi <obi@eoberoi.com>
2022-01-17 21:40:39 -0500

    Apply method contruction

--- DbToJSON/DbToJSON.Api/Utility/FileResultContentTypeOperationFilter.cs
+++ DbToJSON/DbToJSON.Api/Utility/FileResultContentTypeOperationFilter.cs
@@ -10,14 +10,28 @@ namespace DbToJSON.Api.Utility
             var reqAttr = ctx.MethodInfo.GetCustomAttributes(typeof(FileResultContentTypeAttribute), false)
                 .Cast<FileResultContentTypeAttribute>() 
                 .FirstOrDefault();
-            if (reqAttr != null)
-            { 
+            if (reqAttr != null) 
+            {
                 oper.Responses.Clear();
                 oper.Responses.Add("200", new OpenApiResponse
                 {
+                    Content = new Dictionary<string, OpenApiMediaType>()
+                    {
+                        {
+#pragma warning disable CS8604 // Possible null reference argument.
+                            reqAttr.ContentType, new OpenApiMediaType
+#pragma warning restore CS8604 // Possible null reference argument.
+                            {
+                                Schema = new OpenApiSchema
+                                {
+                                    Type = "string",
+                                    Format = "binary"
+                                }
+                            }
+                        }
+                    }
                 });
-            }
-            return;
+            }          
         }
     }
 }
