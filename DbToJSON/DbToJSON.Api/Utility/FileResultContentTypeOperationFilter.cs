9e26e13a6830ff37138df0c5b9f7462e5fc59620
Obi Oberoi <obi@eoberoi.com>
2022-01-17 19:38:24 -0500

    Apply method contruction

--- DbToJSON/DbToJSON.Api/Utility/FileResultContentTypeOperationFilter.cs
+++ DbToJSON/DbToJSON.Api/Utility/FileResultContentTypeOperationFilter.cs
@@ -5,14 +5,14 @@ namespace DbToJSON.Api.Utility
 {
     public class FileResultContentTypeOperationFilter
     {
-        public void Apply(OpenApiOperation op, OperationFilterContext ctx)
+        public void Apply(OpenApiOperation oper, OperationFilterContext ctx)
         { 
             var reqAttr = ctx.MethodInfo.GetCustomAttributes(typeof(FileResultContentTypeAttribute), false)
                 .Cast<FileResultContentTypeAttribute>() 
                 .FirstOrDefault();
             if (reqAttr != null)
             { 
-                
+                oper.Responses.Clear(); 
             }
             return;
         }
