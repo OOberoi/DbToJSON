6245f390dc61d048be1c9159ffeef8e22ac62b8e
Obi Oberoi <obi@eoberoi.com>
2022-01-20 21:55:14 -0500

    implemented the interface

--- DbToJSON/DbToJSON.Identity/Services/AuthenticationService.cs
+++ DbToJSON/DbToJSON.Identity/Services/AuthenticationService.cs
@@ -6,11 +6,21 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using System.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
+using DbToJSON.Application.Contracts.Identity;
+using DbToJSON.Application.Models;
 
 namespace DbToJSON.Identity.Services
 {
-    public class AuthenticationService
+    public class AuthenticationService : IAuthenticationService
     {
+        Task<AuthenticationRequest> IAuthenticationService.AuthenticateAsync(AuthenticationRequest request)
+        {
+            throw new NotImplementedException();
+        }
 
+        Task<RegistrationResponse> IAuthenticationService.RegisterAsync(RegistrationRequest request)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
