a4fc7ccd5eecf0ec8474e84cdd6ede548dd54bd1
Obi Oberoi <obi@eoberoi.com>
2022-01-20 21:27:30 -0500

    added a new authenticated class

---
+++ DbToJSON/DbToJSON.Identity/Services/AuthenticationService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbToJSON.Identity.Services
+{
+    public class AuthenticationService
+    {
+    }
+}
