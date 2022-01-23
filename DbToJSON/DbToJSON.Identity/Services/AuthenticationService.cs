6303af9d3279055227f094a19cac6d32581e46a6
Obi Oberoi <obi@eoberoi.com>
2022-01-22 20:04:15 -0500

    refactored the code slightly

--- DbToJSON/DbToJSON.Identity/Services/AuthenticationService.cs
+++ DbToJSON/DbToJSON.Identity/Services/AuthenticationService.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using DbToJSON.Application.Contracts.Identity;
 using DbToJSON.Application.Models;
 using DbToJSON.Identity.Models;
+using Microsoft.Extensions;
 using DbToJSON.Application.Models.Authentication;
+using Microsoft.Extensions.Options;
 
 namespace DbToJSON.Identity.Services
 {
@@ -21,9 +24,11 @@ namespace DbToJSON.Identity.Services
 
         public AuthenticationService(UserManager<ApplicationUser> userManager,
             SignInManager<ApplicationUser> signInManager,
-            IdentityOptions<JwtSettings> jwSettings)
+            IOptions<JwtSettings> jwSettings)
         {
-
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _jwtSettings = jwSettings.Value;
         }
         Task<AuthenticationRequest> IAuthenticationService.AuthenticateAsync(AuthenticationRequest request)
         {
