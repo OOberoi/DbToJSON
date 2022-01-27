38674cbf55479b95bc80f27035fb14ab0bedae53
Obi Oberoi <obi@eoberoi.com>
2022-01-26 22:39:41 -0500

    installed the logging framework

--- DbToJSON/DbToJSON.Infrastructure/Mail/EmailService.cs
+++ DbToJSON/DbToJSON.Infrastructure/Mail/EmailService.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using SendGrid;
 using DbToJSON.Application.Contracts.Infrastructure;
 using DbToJSON.Application.Models.Mail;
+using Microsoft.Extensions.Logging;
 
 namespace DbToJSON.Infrastructure.Mail
 {
