abbcc8e88c9fe6720c6c778a73032c7fbb4b0f38
Obi Oberoi <obi@eoberoi.com>
2022-01-26 21:40:10 -0500

    organized usings

--- DbToJSON/DbToJSON.Application/Models/Authentication/JwtSettings.cs
+++ DbToJSON/DbToJSON.Application/Models/Authentication/JwtSettings.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DbToJSON.Application.Models.Authentication;
-
-namespace DbToJSON.Application.Models.Authentication
+﻿namespace DbToJSON.Application.Models.Authentication
 {
     public class JwtSettings
     {
