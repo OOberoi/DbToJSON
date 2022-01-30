5a34787389c6d9a2cb86b0998e4ded9622d97e8f
Obi Oberoi <obi@eoberoi.com>
2022-01-30 14:59:50 -0500

    added a new command class

---
+++ DbToJSON/DbToJSON.Application/FeatureSet/Categories/Commands/CreateCategory/CreateCateogoryCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace DbToJSON.Application.FeatureSet.Categories.Commands.CreateCategory
+{
+    internal class CreateCateogoryCommand
+    {
+    }
+}
