0a0eddc3d6ccbca26e16889f73b74ac8e749a1da
Obi Oberoi <obi@eoberoi.com>
2022-01-31 20:59:12 -0500

    adding fluentValidation

--- DbToJSON/DbToJSON.Application/FeatureSet/Categories/Commands/CreateCategory/CreateCategoryCommandValidator.cs
+++ DbToJSON/DbToJSON.Application/FeatureSet/Categories/Commands/CreateCategory/CreateCategoryCommandValidator.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentValidation;
 
 namespace DbToJSON.Application.FeatureSet.Categories.Commands.CreateCategory
 {
-    internal class CreateCategoryCommandValidator
+    public class CreateCategoryCommandValidator
     {
     }
 }
