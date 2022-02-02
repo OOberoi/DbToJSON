39704b68c8584c11cbfe8427af7860dd28e53ea6
Obi Oberoi <obi@eoberoi.com>
2022-02-01 19:50:13 -0500

    added a public getter/setter

---
+++ DbToJSON/DbToJSON.Application/FeatureSet/Categories/Commands/CreateCategory/CreateCategoryCommandResponse.cs
@@ -0,0 +1,17 @@
+﻿using DbToJSON.Application.Responses;
+using DbToJSON.Application;
+
+namespace DbToJSON.Application.FeatureSet.Categories.Commands.CreateCategory
+{
+  
+
+    public class CreateCategoryCommandResponse : BaseResponse
+    {
+        public CreateCategoryDto? CategoryDto { get; set; }
+
+        public CreateCategoryCommandResponse() : base() 
+        {
+
+        }
+    }
+}
