b7db82c71a3bd18c1b85b26c642c9637fe7a78bb
Obi Oberoi <obi@eoberoi.com>
2022-02-02 21:47:01 -0500

    added a new method signature

--- DbToJSON/DbToJSON.Application/Contracts/Persistence/IAsyncRepository.cs
+++ DbToJSON/DbToJSON.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -11,5 +11,7 @@ namespace DbToJSON.Application.Contracts.Persistence
         Task<T> GetByIdAsync(Guid id);
         Task<IReadOnlyList<T>> ListAllAsync();
         Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+
     }
 }
