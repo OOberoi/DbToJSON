bcaa04e5802eb42b3e3350eef337b3aac8501511
Obi Oberoi <obi@eoberoi.com>
2022-02-07 21:38:45 -0500

    added nullable types

--- DbToJSON/BusinessDomain/Entities/Event.cs
+++ DbToJSON/BusinessDomain/Entities/Event.cs
@@ -8,14 +8,14 @@ namespace DbToJSON.BusinessDomain.Entities
 {
     internal class Event
     {
-        public Guid EventId { get; set; }
+        public Guid? EventId { get; set; }
         public string? EventName { get; set; }
         public double? Price  { get; set; }
         public string? Artist { get; set; }
-        public DateTime Date { get; set; }
+        public DateTime? Date { get; set; }
         public string? Description { get; set; }
-        public string Url { get; set; }
-        public Guid CategoryId { get; set; }
+        public string? Url { get; set; }
+        public Guid? CategoryId { get; set; }
         public Category? Category { get; set; }
     }
 }
