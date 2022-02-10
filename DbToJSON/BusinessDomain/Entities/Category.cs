240b54bcf48121c5505b0baf80affde87d2bb658
Obi Oberoi <obi@eoberoi.com>
2022-02-09 21:27:35 -0500

    added the using directive

--- DbToJSON/BusinessDomain/Entities/Category.cs
+++ DbToJSON/BusinessDomain/Entities/Category.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DbToJSON.BusinessDomain.Common;
 
 namespace DbToJSON.BusinessDomain.Entities
 {
-    public class Category
+    public class Category 
     {
         public Guid CategoryId { get; set; }
         public string? Name { get; set; }    
