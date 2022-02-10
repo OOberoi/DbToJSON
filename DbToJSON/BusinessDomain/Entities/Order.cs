dc7b30945690fd1c78b97afb8bb40fd577c9d2fe
Obi Oberoi <obi@eoberoi.com>
2022-02-09 21:30:40 -0500

    added the using directive

--- DbToJSON/BusinessDomain/Entities/Order.cs
+++ DbToJSON/BusinessDomain/Entities/Order.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DbToJSON.BusinessDomain.Common;
 
 namespace DbToJSON.BusinessDomain.Entities
 {
