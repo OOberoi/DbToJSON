102f6e5e9b9d356b20cbc4fb2c0020d374595b21
Obi Oberoi <obi@eoberoi.com>
2022-03-13 17:26:08 -0400

    minor code change

--- DbToJSON/BusinessDomain/Entities/Order.cs
+++ DbToJSON/BusinessDomain/Entities/Order.cs
@@ -12,7 +12,7 @@ namespace DbToJSON.BusinessDomain.Entities
         public Guid Id { get; set; }
         public Guid UserId { get; set; }
         public decimal OrderTotal { get; set; }
-        public DateTime DateOrderPlaced { get; set; }
+        public DateTime? DateOrderPlaced { get; set; }
         public bool? OrderPaid { get; set; }
     }
 }
